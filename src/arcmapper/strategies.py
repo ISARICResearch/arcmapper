7b2a80f4debbf1869521b032b539ad98b946d19d
Abhishek Dasgupta <abhishek.dasgupta@dtc.ox.ac.uk>
2024-08-28 11:04:21 +0100

    Rename to arcmapper

---
+++ src/arcmapper/strategies.py
@@ -0,0 +1,5 @@
+import pandas as pd
+from .types import PossibleMatch
+
+def tf_idf(data_dictionary: pd.DataFrame, arc: pd.DataFrame) -> list[PossibleMatch]:
+    pass
---
+++ src/arcmapper/types.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+from typing import NamedTuple
+
+@dataclass
+class MatchItem:
+    variable: str
+    description: str
+    responses: str
+
+class PossibleMatch(NamedTuple):
+    raw_variable: str
+    raw_response: str
+    arc_variable: str
+    arc_response: str
+    weight: float
+
