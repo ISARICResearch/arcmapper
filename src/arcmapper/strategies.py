1ba7847beb2edf57a005aae516112483dccaa039
Abhishek Dasgupta <abhishek.dasgupta@dtc.ox.ac.uk>
2024-10-23 16:51:38 +0100

    strategies: refactor out common code, add categorical matcher

--- src/arcmapper/strategies.py
+++ src/arcmapper/strategies.py
@@ -4,6 +4,97 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sentence_transformers import SentenceTransformer
 
 
+def get_match_dataframe_from_similarity_matrix(
+    dictionary: pd.DataFrame,
+    arc: pd.DataFrame,
+    similarity_matrix: np.array,
+    num_matches: int,
+    threshold: float,
+) -> pd.DataFrame:
+    """Get mapping matches dataframe from a similarity matrix
+
+    Parameters
+    ----------
+    dictionary
+        Source data dictionary to map
+    arc
+        ARC data dictionary, can be read using :meth:`arcmapper.read_arc_schema`
+    similarity_matrix
+        Similarity matrix calculated using a strategy
+    num_matches
+        Number of matches to return
+    threshold
+        Similarity threshold beyond which a match is reported (upto num_matches).
+        A lower similarity threshold will return more matches, which are potentially
+        incorrect (higher false positive ratio), while a higher threshold will
+        reduce the number of matches, but potentially miss out on correct matches
+        as well (low false positive, higher false negative ratio)
+
+    Returns
+    -------
+    pd.DataFrame
+        Dataframe containing `raw_variable`, `arc_variable` and `rank` columns
+        where `rank` is a number from 0 to num_matches - 1 indicating the fitness
+        of the match, with 0 indicating highest similarity.
+
+    """
+    S = np.argsort(similarity_matrix, axis=1)[:, ::-1][:, :num_matches]
+
+    return pd.DataFrame(
+        columns=["raw_variable", "arc_variable", "rank"],
+        data=sum(
+            [
+                [
+                    [
+                        dictionary.iloc[i].variable,
+                        arc.iloc[k].variable,
+                        j,
+                    ]
+                    for j, k in enumerate(S[i])
+                    if similarity_matrix[i, S[i, j]] > threshold
+                ]
+                for i in range(len(dictionary))
+            ],
+            [],
+        ),
+    )
+
+
+def get_categorical_mapping(
+    source: list[str], target: list[str], similarity_matrix: np.array
+) -> dict[str, str]:
+    """Returns mapping of categorical values from source list to target list.
+    Finds the closest match in target for each string in the source list. This
+    is used to map categorical values from the source dictionary to ARC
+
+    Example: in the source data dictionary, there is a `sex` variable which
+    takes the values `man` and `woman`. ARC has a `demog_sex` variable which
+    takes the values `male`, `female` and `unknown`. Then this function, given
+    an appropriate similarity matrix between [man, woman] and [male, female]
+    would return
+
+    ```json
+    { "man": "male", "woman": "female" }
+    ```
+
+    Parameters
+    ----------
+    source
+        Source list of strings
+    target
+        Target list of strings, usually from the ARC `responses` key
+    similarity_matrix
+        Similarity matrix to use to determine categorical mapping
+
+    Returns
+    -------
+    dict[str, str]
+        Dictionary of source string to target string mappings
+    """
+    max_idx = np.argmax(similarity_matrix, axis=1)
+    return {source[i]: target[max_idx[i]] for i in range(len(source))}
+
+
 def tf_idf(
     dictionary: pd.DataFrame,
     arc: pd.DataFrame,
@@ -39,33 +130,12 @@ def tf_idf(
     ) + dictionary.description.map(lambda x: x if isinstance(x, str) else "")
     arc_text = arc.variable.str.replace("_", " ") + " " + arc.description
     vec = TfidfVectorizer(max_df=0.9, ngram_range=(1, 2))
+
     X = vec.fit_transform(dictionary_text)
     Y = vec.transform(arc_text)
-
-    # Similarity (this is the tf-idf bit.)
-    D = X.dot(Y.T).toarray()
-
-    S = np.argsort(D, axis=1)[:, ::-1][:, :num_matches]
-
-    match_df = pd.DataFrame(
-        columns=["raw_variable", "arc_variable", "rank"],
-        data=sum(
-            [
-                [
-                    [
-                        dictionary.iloc[i].variable,
-                        arc.iloc[k].variable,
-                        j,
-                    ]
-                    for j, k in enumerate(S[i])
-                    if D[i, S[i, j]] > threshold
-                ]
-                for i in range(len(dictionary))
-            ],
-            [],
-        ),
+    return get_match_dataframe_from_similarity_matrix(
+        dictionary, arc, X.dot(Y.T).toarray(), num_matches, threshold
     )
-    return match_df
 
 
 def sbert(
@@ -114,29 +184,13 @@ def sbert(
     )
 
     sbert_model = SentenceTransformer(model)
-
     embeddings = sbert_model.encode(dictionary_text)
     arc_embeddings = sbert_model.encode(arc_text)
 
-    D = sbert_model.similarity(embeddings, arc_embeddings).numpy()
-    S = np.argsort(D, axis=1)[:, ::-1][:, :num_matches]
-
-    match_df = pd.DataFrame(
-        columns=["raw_variable", "arc_variable", "rank"],
-        data=sum(
-            [
-                [
-                    [
-                        dictionary.iloc[i].variable,
-                        arc.iloc[k].variable,
-                        j,
-                    ]
-                    for j, k in enumerate(S[i])
-                    if D[i, S[i, j]] > threshold
-                ]
-                for i in range(len(dictionary))
-            ],
-            [],
-        ),
+    return get_match_dataframe_from_similarity_matrix(
+        dictionary,
+        arc,
+        sbert_model.similarity(embeddings, arc_embeddings).numpy(),
+        num_matches,
+        threshold,
     )
-    return match_df
