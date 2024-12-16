7c33a8550f615f644057d6a529f8d8b586a6ad99
Abhishek Dasgupta <abhishek.dasgupta@dtc.ox.ac.uk>
2024-12-16 17:14:10 +0000

    add mapping progress

---
+++ src/arcmapper/mapping_progress.py
@@ -0,0 +1,13 @@
+"""Mapping progress for ARC to FHIR.
+
+ARC is the data dictionary containing the set of questions with possible
+responses being developed by ISARIC at
+https://github.com/ISARICResearch/DataPlatform/tree/main/ARCH
+
+Each of the ARC variables is mapped to a FHIR resource with the corresponding
+resource elements. FHIR resource descriptions can be obtained at
+https://hl7.org/fhir/resourcelist.html, and the mapping file specification is
+at https://fhirflat.readthedocs.io/en/latest/spec/mapping.html
+"""
+
+
